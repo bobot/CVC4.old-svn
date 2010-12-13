d5ee7763cf91b0a9dc764f3ed2db88db1e567420
Liana Hadarean <lianahady@gmail.com>
2010-12-13 22:44:32 +0000

    adding Ext0 and RoW0 lemmas started adding the union find refactored from UF need to finish check, merge

--- src/theory/arrays/theory_arrays.cpp
+++ src/theory/arrays/theory_arrays.cpp
@@ -33,7 +33,11 @@ using namespace CVC4::theory::arrays;
 TheoryArrays::TheoryArrays(int id, Context* c, OutputChannel& out) :
   Theory(id, c, out),
   d_ccChannel(this),
-  d_cc(c, &d_ccChannel)
+  d_cc(c, &d_ccChannel),
+  d_unionFind(c),
+  d_disequalities(c),
+  d_equalities(c),
+  d_conflict()
 {
 }
 
@@ -51,13 +55,45 @@ void TheoryArrays::addSharedTerm(TNode t) {
 void TheoryArrays::notifyEq(TNode lhs, TNode rhs) {
   Debug("arrays") << "TheoryArrays::notifyEq(): "
                   << lhs << " = " << rhs << endl;
+  // TODO: call the congruence rule if necessary
 }
 
+void TheoryArrays::notifyCongruent(TNode a, TNode b) {
+  Debug("arrays") << "TheoryArrays::notifyCongruent(): "
+       << a << " = " << b << endl;
+}
 
 void TheoryArrays::check(Effort e) {
   while(!done()) {
     Node assertion = get();
     Debug("arrays") << "TheoryArrays::check(): " << assertion << endl;
+
+
+    switch(assertion.getKind()) {
+    case kind::EQUAL:
+    case kind::IFF:
+      d_cc.addEquality(assertion);
+      break;
+    case kind::NOT:
+      if(assertion[0].getKind() == kind::EQUAL ||
+         assertion[0].getKind() == kind::IFF ) {
+
+        Node a = assertion[0][0];
+        Node b = assertion[0][1];
+
+        //TODO: continue
+        //appendToDiseqList(assertion[0]);
+
+        d_cc.addTerm(a);
+        d_cc.addTerm(b);
+
+      }
+      break;
+    default:
+      Unhandled(assertion.getKind());
+    }
+
+
   }
   Debug("arrays") << "TheoryArrays::check(): done" << endl;
 }
@@ -77,4 +113,75 @@ Node TheoryArrays::getValue(TNode n, TheoryEngine* engine) {
   default:
     Unhandled(n.getKind());
   }
+}
+
+
+void TheoryArrays::merge(TNode a, TNode b) {
+  Assert(d_conflict.isNull());
+
+  // make "a" the one with shorter diseqList
+  EqLists::iterator deq_ia = d_disequalities.find(a);
+  EqLists::iterator deq_ib = d_disequalities.find(b);
+
+  if(deq_ia != d_disequalities.end()) {
+    if(deq_ib == d_disequalities.end() ||
+       (*deq_ia).second->size() > (*deq_ib).second->size()) {
+      TNode tmp = a;
+      a = b;
+      b = tmp;
+    }
+  }
+
+  a = find(a);
+  b = find(b);
+
+  if( a == b) {
+    return;
+  }
+
+  d_unionFind.setCanon(a, b);
+
+  EqLists::iterator deq_i = d_disequalities.find(a);
+
+  if(deq_i != d_disequalities.end()) {
+    EqList* deq = (*deq_i).second;
+    for(EqList::const_iterator i = deq->begin(); i!= deq->end(); i++) {
+      /*
+       * Looking for conflicts in the disequality list. Note
+       * that at this point a and b are already merged.
+       */
+      TNode deqn = (*i);
+      Assert(deqn.getKind() == kind::EQUAL || deqn.getKind() == kind::IFF);
+      TNode s = deqn[0];
+      TNode t = deqn[1];
+      if(find(s) == find(t)) {
+        d_conflict = deqn;
+        return;
+      }
+
+    }
+  }
+
+}
+
+
+
+void TheoryArrays::appendToDiseqList(TNode of, TNode eq) {
+  Debug("arrays") << "appending " << eq << endl
+              << "  to diseq list of " << of << endl;
+  Assert(eq.getKind() == kind::EQUAL ||
+         eq.getKind() == kind::IFF);
+
+  EqLists::iterator deq_i = d_disequalities.find(of);
+  EqList* deq;
+  if(deq_i == d_disequalities.end()) {
+    deq = new(getContext()->getCMM()) EqList(true, getContext(), false,
+                                             ContextMemoryAllocator<TNode>(getContext()->getCMM()));
+    d_disequalities.insertDataFromContextMemory(of, deq);
+  } else {
+    deq = (*deq_i).second;
+  }
+
+  deq->push_back(eq);
+
 }
--- src/theory/arrays/theory_arrays.h
+++ src/theory/arrays/theory_arrays.h
@@ -23,6 +23,7 @@
 
 #include "theory/theory.h"
 #include "util/congruence_closure.h"
+#include "theory/arrays/union_find.h"
 
 #include <ext/hash_set>
 #include <iostream>
@@ -56,13 +57,19 @@ private:
   CongruenceClosure<CongruenceChannel, CONGRUENCE_OPERATORS_2 (kind::SELECT, kind::STORE)> d_cc;
 
   /**
+   * Union find for storing the equalities.
+   */
+
+  UnionFind<Node, NodeHashFunction> d_unionFind;
+
+  /**
    * Received a notification from the congruence closure algorithm that the two nodes
    * a and b have been merged.
    */
   void notifyCongruent(TNode a, TNode b);
 
   /**
-   * set of array read terms we care about
+   * set of array read terms we care about a[i]
    */
   std::set<TNode> proxied;
 
@@ -70,6 +77,23 @@ private:
    * set of terms of array type at initialization
    */
   std::set<TNode> array_terms;
+
+  typedef context::CDList<TNode, context::ContextMemoryAllocator<TNode> > EqList;
+  typedef context::CDMap<Node, EqList*, NodeHashFunction> EqLists;
+
+  /**
+   * List of all disequalities this theory has seen.
+   * Maintaints the invariant that if a is in the
+   * disequality list of b, then b is in that of a.
+   * */
+  EqLists d_disequalities;
+
+  /** List of all (potential) equalities to be propagated. */
+  EqLists d_equalities;
+
+  Node d_conflict;
+
+  void appendToDiseqList(TNode of, TNode eq);
 
 public:
   TheoryArrays(int id, context::Context* c, OutputChannel& out);
@@ -81,6 +105,7 @@ public:
     }
 
     if(n.getKind() == kind::STORE || n.getKind() == kind::VARIABLE) {
+      // store all the terms of type ARRAY
       array_terms.insert(n);
     }
   }
@@ -88,17 +113,27 @@ public:
   void registerTerm(TNode n) {
     Debug("arrays-register") << "registering "<< n << std::endl;
     if(n.getKind() == kind::STORE || n.getKind() == kind::VARIABLE) {
-      for(std::set<Node>::iterator i; i != array_terms.end(); i++) {
-        if(*i != n) {
-         // add the Ext0 lemma
+
+      for(std::set<TNode>::iterator it = array_terms.begin(); it != array_terms.end(); it++) {
+        // check that the arrays are of the same type
+        if(*it != n && (*it).getType() == n.getType()) {
+          // add the Ext0 lemma
+          //    for all two arrays a, b of the same type add a != b => a[i]!= b[i]
+          //    for a new variable i.
+
           NodeManager* nm = NodeManager::currentNM();
-          Node neq1 = nm->mkNode(kind::NOT, nm->mkNode(kind::EQUAL, n, *i));
-          Node new_var = nm->mkVar();
+          Node neq1 = nm->mkNode(kind::NOT, nm->mkNode(kind::EQUAL, n, *it));
+          Node new_var = nm->mkVar(n.getType()[0]);
           Node select0 = nm->mkNode(kind::SELECT, n, new_var);
-          Node select1 = nm->mkNode(kind::SELECT, *i, new_var);
+          Node select1 = nm->mkNode(kind::SELECT, *it, new_var);
           Node neq2 = nm->mkNode(kind::NOT, nm->mkNode(kind::EQUAL, select0, select1));
-          Node impl = nm->mkNode(kind::IF, neq1, neq2);
-          out.lemma(impl);
+          Node impl = nm->mkNode(kind::IMPLIES, neq1, neq2);
+
+          d_out->lemma(impl);
+          Debug("arrays-lemma") << "array-lemma "<< impl << std::endl;
+          // add the new terms a[i], b[i] to the list of proxied variables
+          proxied.insert(select0);
+          proxied.insert(select1);
         }
       }
     }
@@ -128,7 +163,26 @@ public:
   Node getValue(TNode n, TheoryEngine* engine);
   void shutdown() { }
   std::string identify() const { return std::string("TheoryArrays"); }
+
+  /**
+   * Merges two congruence classes in the internal
+   * union-find and checks for a conflict.
+   */
+
+  void merge(TNode a, TNode b);
+  inline TNode find(TNode a);
+  inline TNode debugFind(TNode a) const;
+
 };/* class TheoryArrays */
+
+
+inline TNode TheoryArrays::find(TNode a) {
+  return d_unionFind.find(a);
+}
+
+inline TNode TheoryArrays::debugFind(TNode a) const {
+  return d_unionFind.debugFind(a);
+}
 
 }/* CVC4::theory::arrays namespace */
 }/* CVC4::theory namespace */
