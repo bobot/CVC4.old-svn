a7b21614d4a04c0cd340318ddd8bb90e31b30778
Morgan Deters <mdeters@gmail.com>
2011-10-21 05:20:54 +0000

    more printer fixes

--- src/printer/smt2/smt2_printer.cpp
+++ src/printer/smt2/smt2_printer.cpp
@@ -70,7 +70,7 @@ void Smt2Printer::toStream(std::ostream& out, TNode n,
     switch(n.getKind()) {
     case kind::TYPE_CONSTANT:
       switch(n.getConst<TypeConstant>()) {
-      case BOOLEAN_TYPE: out << "Boolean"; break;
+      case BOOLEAN_TYPE: out << "Bool"; break;
       case REAL_TYPE: out << "Real"; break;
       case INTEGER_TYPE: out << "Int"; break;
       default:
@@ -103,7 +103,7 @@ void Smt2Printer::toStream(std::ostream& out, TNode n,
     case kind::CONST_INTEGER: {
       Integer i = n.getConst<Integer>();
       if(i < 0) {
-        out << "(- " << i << ')';
+        out << "(- " << -i << ')';
       } else {
         out << i;
       }
@@ -112,7 +112,7 @@ void Smt2Printer::toStream(std::ostream& out, TNode n,
     case kind::CONST_RATIONAL: {
       Rational r = n.getConst<Rational>();
       if(r < 0) {
-        out << "(- " << r << ')';
+        out << "(- " << -r << ')';
       } else {
         out << r;
       }
