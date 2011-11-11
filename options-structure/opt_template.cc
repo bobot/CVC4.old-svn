adecc2ea0c894dc63d62ce9f9b40aeae5363f37a
Morgan Deters <mdeters@gmail.com>
2011-11-11 22:23:03 +0000

    save pt

---
+++ options-structure/opt_template.cc
@@ -0,0 +1,8 @@
+#include "t_opt.h"
+#include "options_holder.h"
+
+namespace CVC4 {
+
+${module_accessors}
+
+}
---
+++ options-structure/opt_template.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "options.h"
+
+#define ${module_id}_OPTIONS ${module_optionholder_spec}
+
+namespace CVC4 {
+
+${module_decls}
+
+${module_specializations}
+
+}
+
