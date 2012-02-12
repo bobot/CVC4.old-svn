357e76c043c3f1ad3e9183309a599486e81dcee6
Morgan Deters <mdeters@gmail.com>
2012-02-12 19:55:13 +0000

    fix parser makefile

--- src/parser/antlr_input.h
+++ src/parser/antlr_input.h
@@ -89,7 +89,7 @@ public:
   static AntlrInputStream* newStringInputStream(const std::string& input, 
                                                 const std::string& name)
     throw (InputStreamException, AssertionException);
-};
+};/* class AntlrInputStream */
 
 class Parser;
 
@@ -210,7 +210,7 @@ protected:
 
   /** Set the Parser object for this input. */
   virtual void setParser(Parser& parser);
-};
+};/* class AntlrInput */
 
 inline std::string AntlrInput::getUnparsedText() {
   const char *base = (const char *)d_antlr3InputStream->data;
