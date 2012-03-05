8c6a221209e6fa661a19b59da88f26f81f4e3ff1
Liana Hadarean <lianahady@gmail.com>
2012-03-05 23:59:42 +0000

    forgot files, again

---
+++ src/theory/bv/theory_bv_rewrite_rules_simplification.h
@@ -0,0 +1,46 @@
+/*********************                                                        */
+/*! \file theory_bv_rewrite_rules_simplification.h
+ ** \verbatim
+ ** Original author: lianah
+ ** Major contributors: none
+ ** Minor contributors (to current version): 
+ ** This file is part of the CVC4 prototype.
+ ** Copyright (c) 2009, 2010, 2011  The Analysis of Computer Systems Group (ACSys)
+ ** Courant Institute of Mathematical Sciences
+ ** New York University
+ ** See the file COPYING in the top-level source directory for licensing
+ ** information.\endverbatim
+ **
+ ** \brief [[ Add one-line brief description here ]]
+ **
+ ** [[ Add lengthier description here ]]
+ ** \todo document this file
+ **/
+
+#include "cvc4_private.h"
+
+#pragma once
+
+#include "theory/bv/theory_bv_rewrite_rules.h"
+#include "theory/bv/theory_bv_utils.h"
+
+namespace CVC4 {
+namespace theory {
+namespace bv {
+
+// template<>
+// bool RewriteRule<>::applies(Node node) {
+//   return (node.getKind() == kind::BITVECTOR_CONCAT);
+// }
+
+// template<>
+// Node RewriteRule<>::apply(Node node) {
+//   BVDebug("bv-rewrite") << "RewriteRule<>(" << node << ")" << std::endl;
+//   return resultNode;
+// }
+
+
+
+}
+}
+}
