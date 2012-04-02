da1f68e62ec692ca10ea0dc4b52cc26f88b273ca
Kshitij Bansal <kshitij@cs.nyu.edu>
2012-04-02 20:48:50 +0000

    a hack in simp solve

--- src/prop/minisat/simp/SimpSolver.cc
+++ src/prop/minisat/simp/SimpSolver.cc
@@ -138,10 +138,10 @@ lbool SimpSolver::solve_(bool do_simp, bool turn_off_simp)
 
 bool SimpSolver::addClause_(vec<Lit>& ps, bool removable)
 {
-#ifndef NDEBUG
-    for (int i = 0; i < ps.size(); i++)
-        assert(!isEliminated(var(ps[i])));
-#endif
+// #ifndef NDEBUG
+//     for (int i = 0; i < ps.size(); i++)
+//         assert(!isEliminated(var(ps[i])));
+// #endif
 
     int nclauses = clauses_persistent.size();
 
