1ab320c953bc7f3d37103843698cf74f19c97a1d
Kshitij Bansal <kshitij@cs.nyu.edu>
2012-04-18 08:50:44 +0000

    merged r3102:3110 from trunk

--- test/unit/context/cdlist_black.h
+++ test/unit/context/cdlist_black.h
@@ -136,15 +136,10 @@ public:
     TS_ASSERT_EQUALS(aThirdFalse, false);
   }
 
-  void testEmptyIterators() {
-    CDList<int>* list1 = new(true) CDList<int>(d_context);
-    CDList< int, ContextMemoryAllocator<int> >* list2 =
-      new(d_context->getCMM())
-        CDList< int, ContextMemoryAllocator<int> >(true, d_context, false,
-                                                   ContextMemoryAllocator<int>(d_context->getCMM()));
-    TS_ASSERT_EQUALS(list1->begin(), list1->end());
-    TS_ASSERT_EQUALS(list2->begin(), list2->end());
-    list1->deleteSelf();
+  void testEmptyIterator() {
+    CDList<int>* list = new(true) CDList<int>(d_context);
+    TS_ASSERT_EQUALS(list->begin(), list->end());
+    list->deleteSelf();
   }
 
   /* setrlimit() totally broken on Mac OS X */
