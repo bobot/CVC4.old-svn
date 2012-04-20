b0b4adbddf84f1640da3a3c0d51c59ed18fc06de
Tim King <taking@cs.nyu.edu>
2012-04-20 15:48:13 +0000

    This branch is to improve propagation of conrgruent shared terms.

---
+++ congruence/src/theory/substitutions.cpp
@@ -0,0 +1,181 @@
+/*********************                                                        */
+/*! \file substitutions.cpp
+ ** \verbatim
+ ** Original author: dejan
+ ** Major contributors: none
+ ** Minor contributors (to current version): none
+ ** This file is part of the CVC4 prototype.
+ ** Copyright (c) 2009, 2010, 2011  The Analysis of Computer Systems Group (ACSys)
+ ** Courant Institute of Mathematical Sciences
+ ** New York University
+ ** See the file COPYING in the top-level source directory for licensing
+ ** information.\endverbatim
+ **
+ ** \brief A substitution mapping for theory simplification
+ **
+ ** A substitution mapping for theory simplification.
+ **/
+
+#include "theory/substitutions.h"
+
+using namespace std;
+
+namespace CVC4 {
+namespace theory {
+
+struct substitution_stack_element {
+  TNode node;
+  bool children_added;
+  substitution_stack_element(TNode node)
+  : node(node), children_added(false) {}
+};
+
+Node SubstitutionMap::internalSubstitute(TNode t, NodeCache& substitutionCache) {
+
+  Debug("substitution::internal") << "SubstitutionMap::internalSubstitute(" << t << ")" << std::endl;
+
+  // If nothing to substitute just return the node
+  if (substitutionCache.empty()) {
+    return t;
+  }
+
+  // Do a topological sort of the subexpressions and substitute them
+  vector<substitution_stack_element> toVisit;
+  toVisit.push_back((TNode) t);
+
+  while (!toVisit.empty())
+  {
+    // The current node we are processing
+    substitution_stack_element& stackHead = toVisit.back();
+    TNode current = stackHead.node;
+
+    Debug("substitution::internal") << "SubstitutionMap::internalSubstitute(" << t << "): processing " << current << std::endl;
+
+    // If node already in the cache we're done, pop from the stack
+    NodeCache::iterator find = substitutionCache.find(current);
+    if (find != substitutionCache.end()) {
+      toVisit.pop_back();
+      continue;
+    }
+
+    // Not yet substituted, so process
+    if (stackHead.children_added) {
+      // Children have been processed, so substitute
+      NodeBuilder<> builder(current.getKind());
+      if (current.getMetaKind() == kind::metakind::PARAMETERIZED) {
+        builder << current.getOperator();
+      }
+      for (unsigned i = 0; i < current.getNumChildren(); ++ i) {
+        Assert(substitutionCache.find(current[i]) != substitutionCache.end());
+        builder << Node(substitutionCache[current[i]]);
+      }
+      // Mark the substitution and continue
+      Node result = builder;
+      Debug("substitution::internal") << "SubstitutionMap::internalSubstitute(" << t << "): setting " << current << " -> " << result << std::endl;
+      substitutionCache[current] = result;
+      toVisit.pop_back();
+    } else {
+      // Mark that we have added the children if any
+      if (current.getNumChildren() > 0) {
+        stackHead.children_added = true;
+        // We need to add the children
+        for(TNode::iterator child_it = current.begin(); child_it != current.end(); ++ child_it) {
+          TNode childNode = *child_it;
+          NodeCache::iterator childFind = substitutionCache.find(childNode);
+          if (childFind == substitutionCache.end()) {
+            toVisit.push_back(childNode);
+          }
+        }
+      } else {
+        // No children, so we're done
+        Debug("substitution::internal") << "SubstitutionMap::internalSubstitute(" << t << "): setting " << current << " -> " << current << std::endl;
+        substitutionCache[current] = current;
+        toVisit.pop_back();
+      }
+    }
+  }
+
+  // Return the substituted version
+  return substitutionCache[t];
+}
+
+void SubstitutionMap::addSubstitution(TNode x, TNode t, bool invalidateCache) {
+  Debug("substitution") << "SubstitutionMap::addSubstitution(" << x << ", " << t << ")" << std::endl;
+  Assert(d_substitutions.find(x) == d_substitutions.end());
+
+  // Temporary substitution cache
+  NodeCache tempCache;
+  tempCache[x] = t;
+
+  // Put in the new substitutions into the old ones
+  NodeMap::iterator it = d_substitutions.begin();
+  NodeMap::iterator it_end = d_substitutions.end();
+  for(; it != it_end; ++ it) {
+    d_substitutions[(*it).first] = internalSubstitute((*it).second, tempCache);
+  }
+
+  // Put the new substitution in
+  d_substitutions[x] = t;
+
+  // Also invalidate the cache
+  if (invalidateCache) {
+    d_cacheInvalidated = true;
+  }
+}
+
+static bool check(TNode node, const SubstitutionMap::NodeMap& substitutions) CVC4_UNUSED;
+static bool check(TNode node, const SubstitutionMap::NodeMap& substitutions) {
+  SubstitutionMap::NodeMap::const_iterator it = substitutions.begin();
+  SubstitutionMap::NodeMap::const_iterator it_end = substitutions.end();
+  Debug("substitution") << "checking " << node << std::endl;
+  for (; it != it_end; ++ it) {
+    Debug("substitution") << "-- hasSubterm( " << (*it).first << " ) ?" << std::endl;
+    if (node.hasSubterm((*it).first)) {
+      Debug("substitution") << "-- FAIL" << std::endl;
+      return false;
+    }
+  }
+  Debug("substitution") << "-- SUCCEED" << std::endl;
+  return true;
+}
+
+Node SubstitutionMap::apply(TNode t) {
+
+  Debug("substitution") << "SubstitutionMap::apply(" << t << ")" << std::endl;
+
+  // Setup the cache
+  if (d_cacheInvalidated) {
+    SubstitutionMap::NodeMap::const_iterator it = d_substitutions.begin();
+    SubstitutionMap::NodeMap::const_iterator it_end = d_substitutions.end();
+    d_substitutionCache.clear();
+    for (; it != it_end; ++ it) {
+      d_substitutionCache[(*it).first] = (*it).second;
+    }
+    d_cacheInvalidated = false;
+    Debug("substitution") << "-- reset the cache" << std::endl;
+  }
+
+  // Perform the substitution
+  Node result = internalSubstitute(t, d_substitutionCache);
+  Debug("substitution") << "SubstitutionMap::apply(" << t << ") => " << result << std::endl;
+
+  Assert(check(result, d_substitutions));
+
+  return result;
+}
+
+void SubstitutionMap::print(ostream& out) const {
+  NodeMap::const_iterator it = d_substitutions.begin();
+  NodeMap::const_iterator it_end = d_substitutions.end();
+  for (; it != it_end; ++ it) {
+    out << (*it).first << " -> " << (*it).second << endl;
+  }
+}
+
+}/* CVC4::theory namespace */
+
+std::ostream& operator<<(std::ostream& out, const theory::SubstitutionMap::iterator& i) {
+  return out << "[CDMap-iterator]";
+}
+
+}/* CVC4 namespace */
---
+++ congruence/src/util/options.h
@@ -0,0 +1,354 @@
+/*********************                                                        */
+/*! \file options.h
+ ** \verbatim
+ ** Original author: mdeters
+ ** Major contributors: taking, cconway
+ ** Minor contributors (to current version): dejan
+ ** This file is part of the CVC4 prototype.
+ ** Copyright (c) 2009, 2010, 2011  The Analysis of Computer Systems Group (ACSys)
+ ** Courant Institute of Mathematical Sciences
+ ** New York University
+ ** See the file COPYING in the top-level source directory for licensing
+ ** information.\endverbatim
+ **
+ ** \brief Global (command-line, set-option, ...) parameters for SMT.
+ **
+ ** Global (command-line, set-option, ...) parameters for SMT.
+ **/
+
+#include "cvc4_public.h"
+
+#ifndef __CVC4__OPTIONS_H
+#define __CVC4__OPTIONS_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "util/exception.h"
+#include "util/language.h"
+#include "util/lemma_output_channel.h"
+#include "util/lemma_input_channel.h"
+#include "util/tls.h"
+
+#include <vector>
+
+namespace CVC4 {
+
+class ExprStream;
+
+/** Class representing an option-parsing exception. */
+class CVC4_PUBLIC OptionException : public CVC4::Exception {
+public:
+    OptionException(const std::string& s) throw() :
+      CVC4::Exception("Error in option parsing: " + s) {
+    }
+};/* class OptionException */
+
+struct CVC4_PUBLIC Options {
+
+  /** The current Options in effect */
+  static CVC4_THREADLOCAL(const Options*) s_current;
+
+  /** Get the current Options in effect */
+  static inline const Options* current() {
+    return s_current;
+  }
+
+  /** The name of the binary (e.g. "cvc4") */
+  std::string binary_name;
+
+  /** Whether to collect statistics during this run */
+  bool statistics;
+
+  std::istream* in; /*< The input stream to use */
+  std::ostream* out; /*< The output stream to use */
+  std::ostream* err; /*< The error stream to use */
+
+  /* -1 means no output */
+  /* 0 is normal (and default) -- warnings only */
+  /* 1 is warnings + notices so the user doesn't get too bored */
+  /* 2 is chatty, giving statistical things etc. */
+  /* with 3, the solver is slowed down by all the scrolling */
+  int verbosity;
+
+  /** The input language */
+  InputLanguage inputLanguage;
+
+  /** The output language */
+  OutputLanguage outputLanguage;
+
+  /** Should we print the help message? */
+  bool help;
+
+  /** Should we print the release information? */
+  bool version;
+
+  /** Should we print the language help information? */
+  bool languageHelp;
+
+  /** Should we exit after parsing? */
+  bool parseOnly;
+
+  /** Should we exit after preprocessing? */
+  bool preprocessOnly;
+
+  /** Should the parser do semantic checks? */
+  bool semanticChecks;
+
+  /** Should the TheoryEngine do theory registration? */
+  bool theoryRegistration;
+
+  /** Should the parser memory-map file input? */
+  bool memoryMap;
+
+  /** Should we strictly enforce the language standard while parsing? */
+  bool strictParsing;
+
+  /** Should we expand function definitions lazily? */
+  bool lazyDefinitionExpansion;
+
+  /** Parallel Only: Whether the winner is printed at the end or not. */
+  bool printWinner;
+
+  /** Enumeration of simplification modes (when to simplify). */
+  typedef enum {
+    /** Simplify the assertions as they come in */
+    SIMPLIFICATION_MODE_INCREMENTAL,
+    /** Simplify the assertions all together once a check is requested */
+    SIMPLIFICATION_MODE_BATCH,
+    /** Don't do simplification */
+    SIMPLIFICATION_MODE_NONE
+  } SimplificationMode;
+
+  /** When/whether to perform nonclausal simplifications. */
+  SimplificationMode simplificationMode;
+  /** Whether the user set the nonclausal simplification mode. */
+  bool simplificationModeSetByUser;
+
+  /** Enumeration of decision strategies */
+  typedef enum {
+    /**
+     * Decision engine doesn't do anything. Use sat solver's internal
+     * heuristics
+     */
+    DECISION_STRATEGY_INTERNAL,
+    /**
+     * Use the justification heuristic
+     */
+    DECISION_STRATEGY_JUSTIFICATION
+  } DecisionMode;
+  /** When/whether to use any decision strategies */
+  DecisionMode decisionMode;
+  /** Whether the user set the decision strategy */
+  bool decisionModeSetByUser;
+
+  /** Whether to perform the static learning pass. */
+  bool doStaticLearning;
+
+  /** Whether we're in interactive mode or not */
+  bool interactive;
+
+  /**
+   * Whether we're in interactive mode (or not) due to explicit user
+   * setting (if false, we inferred the proper default setting).
+   */
+  bool interactiveSetByUser;
+
+  /** Per-query resource limit. */
+  unsigned long perCallResourceLimit;
+  /** Cumulative resource limit. */
+  unsigned long cumulativeResourceLimit;
+
+  /** Per-query time limit in milliseconds. */
+  unsigned long perCallMillisecondLimit;
+  /** Cumulative time limit in milliseconds. */
+  unsigned long cumulativeMillisecondLimit;
+
+  /** Whether we should "spin" on a SIG_SEGV. */
+  bool segvNoSpin;
+
+  /** Whether we support SmtEngine::getValue() for this run. */
+  bool produceModels;
+
+  /** Whether we produce proofs. */
+  bool proof;
+
+  /** Whether we support SmtEngine::getAssignment() for this run. */
+  bool produceAssignments;
+
+  /** Whether we do typechecking at all. */
+  bool typeChecking;
+
+  /** Whether we do typechecking at Expr creation time. */
+  bool earlyTypeChecking;
+
+  /** Whether incemental solving (push/pop) */
+  bool incrementalSolving;
+
+  /** Replay file to use (for decisions); empty if no replay file. */
+  std::string replayFilename;
+
+  /** Replay stream to use (for decisions); NULL if no replay file. */
+  ExprStream* replayStream;
+
+  /** Log to write replay instructions to; NULL if not logging. */
+  std::ostream* replayLog;
+
+  /** Turn on and of arithmetic propagation. */
+  bool arithPropagation;
+
+  /**
+   * Frequency for the sat solver to make random decisions.
+   * Should be between 0 and 1.
+   */
+  double satRandomFreq;
+
+  /**
+   * Seed for Minisat's random decision procedure.
+   * If this is 0, no random decisions will occur.
+   **/
+  double satRandomSeed;
+
+  /** Variable activity decay factor for Minisat */
+  double satVarDecay;
+
+  /** Clause activity decay factor for Minisat */
+  double satClauseDecay;
+
+  /** Base restart interval for Minisat */
+  int satRestartFirst;
+
+  /** Restart interval increase factor for Minisat */
+  double satRestartInc;
+
+  /** The pivot rule for arithmetic */
+  typedef enum { MINIMUM, BREAK_TIES, MAXIMUM } ArithPivotRule;
+  ArithPivotRule pivotRule;
+
+  /**
+   * The number of pivots before Bland's pivot rule is used on a basic
+   * variable in arithmetic.
+   */
+  uint16_t arithPivotThreshold;
+
+  /**
+   * The maximum row length that arithmetic will use for propagation.
+   */
+  uint16_t arithPropagateMaxLength;
+
+  /**
+   * Whether to do the symmetry-breaking preprocessing in UF as
+   * described by Deharbe et al. in CADE 2011 (on by default).
+   */
+  bool ufSymmetryBreaker;
+
+  /**
+   * Whether the user explicitly requested that the symmetry
+   * breaker be enabled or disabled.
+   */
+  bool ufSymmetryBreakerSetByUser;
+
+  /**
+   * Whether to do the linear diophantine equation solver
+   * in Arith as described by Griggio JSAT 2012 (on by default).
+   */
+  bool dioSolver;
+
+  /**
+   * Whether to split (= x y) into (and (<= x y) (>= x y)) in
+   * arithmetic preprocessing.
+   */
+  bool arithRewriteEq;
+
+  /** The output channel to receive notfication events for new lemmas */
+  LemmaOutputChannel* lemmaOutputChannel;
+  LemmaInputChannel* lemmaInputChannel;
+
+  /** Total number of threads */
+  int threads;
+
+  /** Thread configuration (a string to be passed to parseOptions) */
+  std::vector<std::string> threadArgv;
+
+  /** Thread ID, for internal use in case of multi-threaded run */
+  int thread_id;
+
+  /**
+   * In multi-threaded setting print output of each thread at the
+   * end of run, separated by a divider ("----").
+   **/
+  bool separateOutput;
+
+  /** Filter depending on length of lemma */
+  int sharingFilterByLength;
+
+  Options();
+
+  /**
+   * Get a description of the command-line flags accepted by
+   * parseOptions.  The returned string will be escaped so that it is
+   * suitable as an argument to printf. */
+  std::string getDescription() const;
+
+  /**
+   * Print overall command-line option usage message, prefixed by
+   * "msg"---which could be an error message causing the usage
+   * output in the first place, e.g. "no such option --foo"
+   */
+  static void printUsage(const std::string msg, std::ostream& out);
+
+  /**
+   * Print command-line option usage message for only the most-commonly
+   * used options.  The message is prefixed by "msg"---which could be
+   * an error message causing the usage output in the first place, e.g.
+   * "no such option --foo"
+   */
+  static void printShortUsage(const std::string msg, std::ostream& out);
+
+  /** Print help for the --lang command line option */
+  static void printLanguageHelp(std::ostream& out);
+
+  /**
+   * Initialize the options based on the given command-line arguments.
+   */
+  int parseOptions(int argc, char* argv[]) throw(OptionException);
+
+  /**
+   * Set the output language based on the given string.
+   */
+  void setOutputLanguage(const char* str) throw(OptionException);
+
+  /**
+   * Set the input language based on the given string.
+   */
+  void setInputLanguage(const char* str) throw(OptionException);
+
+};/* struct Options */
+
+inline std::ostream& operator<<(std::ostream& out,
+                                Options::SimplificationMode mode) CVC4_PUBLIC;
+inline std::ostream& operator<<(std::ostream& out,
+                                Options::SimplificationMode mode) {
+  switch(mode) {
+  case Options::SIMPLIFICATION_MODE_INCREMENTAL:
+    out << "SIMPLIFICATION_MODE_INCREMENTAL";
+    break;
+  case Options::SIMPLIFICATION_MODE_BATCH:
+    out << "SIMPLIFICATION_MODE_BATCH";
+    break;
+  case Options::SIMPLIFICATION_MODE_NONE:
+    out << "SIMPLIFICATION_MODE_NONE";
+    break;
+  default:
+    out << "SimplificationMode:UNKNOWN![" << unsigned(mode) << "]";
+  }
+
+  return out;
+}
+
+std::ostream& operator<<(std::ostream& out, Options::ArithPivotRule rule) CVC4_PUBLIC;
+
+}/* CVC4 namespace */
+
+#endif /* __CVC4__OPTIONS_H */
