e32f8a15319b40d6687e18192fe365745ae48aa5
Morgan Deters <mdeters@gmail.com>
2012-05-11 19:25:13 +0000

    merging r3438 selectively from trunk; adds a warning when Boolean terms are detected

--- src/expr/node_manager.h
+++ src/expr/node_manager.h
@@ -959,6 +959,9 @@ NodeManager::mkFunctionType(const std::vector<TypeNode>& sorts) {
   for (unsigned i = 0; i < sorts.size(); ++ i) {
     CheckArgument(!sorts[i].isFunctionLike(), sorts,
                   "cannot create higher-order function types");
+    if(i + 1 < sorts.size() && sorts[i].isBoolean()) {
+      WarningOnce() << "Warning: CVC4 does not yet support Boolean terms (you have created a function type with a Boolean argument)" << std::endl;
+    }
     sortNodes.push_back(sorts[i]);
   }
   return mkTypeNode(kind::FUNCTION_TYPE, sortNodes);
@@ -971,6 +974,9 @@ NodeManager::mkPredicateType(const std::vector<TypeNode>& sorts) {
   for (unsigned i = 0; i < sorts.size(); ++ i) {
     CheckArgument(!sorts[i].isFunctionLike(), sorts,
                   "cannot create higher-order function types");
+    if(i + 1 < sorts.size() && sorts[i].isBoolean()) {
+      WarningOnce() << "Warning: CVC4 does not yet support Boolean terms (you have created a predicate type with a Boolean argument)" << std::endl;
+    }
     sortNodes.push_back(sorts[i]);
   }
   sortNodes.push_back(booleanType());
@@ -985,6 +991,9 @@ inline TypeNode NodeManager::mkTupleType(const std::vector<TypeNode>& types) {
 #if 0
     CheckArgument(!types[i].isFunctionLike(), types,
                   "cannot put function-like types in tuples");
+    if(types[i].isBoolean()) {
+      WarningOnce() << "Warning: CVC4 does not yet support Boolean terms (you have created a tuple type with a Boolean argument)" << std::endl;
+    }
 #endif /* 0 */
     typeNodes.push_back(types[i]);
   }
@@ -1005,7 +1014,10 @@ inline TypeNode NodeManager::mkArrayType(TypeNode indexType,
                 "cannot index arrays by a function-like type");
   CheckArgument(!constituentType.isFunctionLike(), constituentType,
                 "cannot store function-like types in arrays");
-Debug("arrays") << "making array type " << indexType << " " << constituentType << std::endl;
+  if(indexType.isBoolean() || constituentType.isBoolean()) {
+    WarningOnce() << "Warning: CVC4 does not yet support Boolean terms (you have created an array type with a Boolean index or constituent type)" << std::endl;
+  }
+  Debug("arrays") << "making array type " << indexType << " " << constituentType << std::endl;
   //std::cout << "arrays ";
   //exit( 7 );
   return mkTypeNode(kind::ARRAY_TYPE, indexType, constituentType);
