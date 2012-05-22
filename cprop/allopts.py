cacf12bf0c2a5f6ac78ea9817921a6dc353acfd1
Tim King <taking@cs.nyu.edu>
2012-05-22 14:13:16 +0000

    Merging trunk upto 3507 into cprop.

--- cprop/allopts.py
+++ cprop/allopts.py
@@ -2,7 +2,8 @@
 #try me: python allopts.py ~/benchmarks/smtlib2/QF_LRA/tta_startup/simple_startup_6nodes.abstract.induct.smt2 &> res ; grep "running\|user\|THEORY_ARITH\|sat/unsat" res > sel
 
 import sys
-import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import multiprocessing
 import itertools
 import string
 
@@ -10,28 +11,41 @@ print sys.argv
 
 assert len(sys.argv) > 1
 
+binary = "/home/taking/ws/cvc4/branches/arithmetic/cprop/builds/bin/cvc4"
+processes = 3
+
 examples = sys.argv[1:]
 optionsChoices = itertools.product(
+    ["time"],
+    [binary],
+    ["--stats"],
+    ["--disable-arith-rewrite-equalities",
+     "--enable-arith-rewrite-equalities"],
+    ["--arith-prop=none",
+     "--arith-prop=new",
+     "--arith-prop=old",
+     "--arith-prop=both"],
     ["--unate-lemmas all",
      "--unate-lemmas none",
      "--unate-lemmas ineqs",
-     "--unate-lemmas eqs"],
-    ["",
-     "--disable-arith-rewrite-equalities"])
+     "--unate-lemmas eqs"])
 
 options = [string.join(o) for o in optionsChoices ]
 
+runChoices = itertools.product(options, examples)
+runs = [string.join(r) for r in runChoices ]
+
 print options
+print runs
 
-binary = "/home/taking/ws/cvc4/branches/arithmetic/cprop/builds/bin/cvc4"
-fixedOptions = "--stats"
-run = "time " + binary +" "+fixedOptions+" "
+def runCommandline(run):
+    output = "running: " + run + "\n"
+    p = Popen(run, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    output += p.stdout.read()
+    return output
 
-for e in examples:
-    for opt in options:
-        run = "time " + binary +" "+fixedOptions+" " + opt + " " + e
-        print "running:", run
-        sys.stdout.flush()
-        subprocess.call(run, shell=True)
-        sys.stdout.flush()
-        sys.stderr.flush()
+p = multiprocessing.Pool(processes)
+res = p.map(runCommandline, runs)
+
+for r in res:
+    print r
