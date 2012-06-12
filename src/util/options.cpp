445de44f81f6a44fbc69bbc410041eee63a69b5e
Kshitij Bansal <kshitij@cs.nyu.edu>
2012-06-12 14:34:01 +0000

    make regress pass

--- src/util/options.cpp
+++ src/util/options.cpp
@@ -88,7 +88,7 @@ Options::Options() :
   printWinner(false),
   simplificationMode(SIMPLIFICATION_MODE_BATCH),
   simplificationModeSetByUser(false),
-  decisionMode(DECISION_STRATEGY_INTERNAL),
+  decisionMode(DECISION_STRATEGY_JUSTIFICATION),
   decisionModeSetByUser(false),
   decisionOptions(DecisionOptions(defaultDecOpt)),
   doStaticLearning(true),
